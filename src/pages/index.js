760883e675ccc6e1bee081a53606ffe3361d92e6
YutaFukuda <fukuda@makersmark.co.jp>
2020-06-28 13:15:50 +0900

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
 
+const hello="こんにちは"
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>挨拶</h1>
+    <h2 className="japan">日本語</h2>
+    <p style={{color:"#eee"}}>{hello}</p>
+    <p><img src="/images/about.jpg" alt="イメージ"/></p>
+    <footer>
+      Presented by Moniker
+    </footer>
+  </div>
 }
